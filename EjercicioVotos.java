9f18d8b485784c39a3247bc65a7503b28b7aa11a
unknown <dubantique5@gmail.com>
2026-08-28 10:59:55 -0500

    practica con ejercicio de votos inicio de definicion de variables

---
+++ EjercicioVotos.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class VotosPractica {
+
+public static void Main(String[] args) 
+Scanner leer = new Scanner (System.in);
+
+int servicio1 = 0;
+int servicio2 = 0;
+int servicio3 = 0;
+int terminarEncuesta4 = 0;
+int votos = 0;
+int opcion = 0;
+double porcentajeVoto = 0.0;
+
+while (opcion !=4) {
+   System.out.println("SELECCIONE UNA OPCION");
+   System.out.println("Servicio 1 EXCELENTE");
+   System.out.println("Servicio 2  REGULAR");
+   System.out.println("Servicio 3 MALO");
+   System.out.println("Finalizar Encuesta 4");
+};
